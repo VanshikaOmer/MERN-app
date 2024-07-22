0e6ea9ecbde1ce4716e0691b618ea42722e8b7af
VanshikaOmer <omer.vanshika@gmail.com>
2024-07-22 08:16:06 +0530

    Initial commit for MERN app/

---
+++ my-mern-project/frontend/src/index.js
@@ -0,0 +1,16 @@
+// frontend/src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// Optional: If you are using reportWebVitals for performance monitoring
+reportWebVitals();
